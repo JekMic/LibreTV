2a3513d7237771c321cf641c22b050e1bccf4bd6
JekMic <JekMic@outlook.com>
2025-08-06 14:56:04 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -114,41 +114,9 @@ const API_SITES = {
         api: 'https://cj.lziapi.com/api.php/provide/vod/',
         name: '量子资源站'
     },
-    jszy: {
-        api: 'https://jszyapi.com/api.php/provide/vod',
-        name: '极速资源'
-    },
-    hongniu: {
-            api: 'https://www.hongniuzy2.com/api.php/provide/vod',
-            name: '红牛'
-        },
-    91md: {
-            api: 'https://91md.me/api.php/provide/vod',
-            name: '91麻豆'
-        },
-    madou: {
-            api: 'https://madouse.la/api.php/provide/vod',
-            name: '麻豆'
-        },
-    lbzy: {
-            api: 'https://lbapi9.com/api.php/provide/vod',
-            name: '乐播资源'
-        },
-    danaizi: {
-            api: 'https://apidanaizi.com/api.php/provide/vod',
-            name: '大奶子'
-        },
-    xingba: {
-            api: 'xingba222.com/api.php/provide/vod',
-            name: '杏吧'
-        },
-    lsb: {
-            api: 'https://apilsbzy1.com/api.php/provide/vod',
-            name: '老色逼'
-        },
     testSource: {
-        api: 'https://apilsbzy1.com/api.php/provide/vod',
-        name: 'test',
+        api: 'https://www.example.com/api.php/provide/vod',
+        name: '空内容测试源',
         adult: true
     },
 };
